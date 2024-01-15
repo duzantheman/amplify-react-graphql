f82169c34e3ac72f1f39ff2e35a59861d81cbd45
Chris Duzan <cgduzan@gmail.com>
2024-01-14 21:02:43 -0500

    Added storage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
-// import { API } from "aws-amplify";
 import { generateClient } from 'aws-amplify/api';
+// import { Storage } from "aws-amplify";
+import { getUrl, uploadData, remove } from 'aws-amplify/storage';
 import {
   Button,
   Flex,
   Heading,
+  Image,
   Text,
   TextField,
   View,
@@ -30,16 +32,55 @@ const App = ({ signOut }) => {
   async function fetchNotes() {
     const apiData = await client.graphql({ query: listNotes });
     const notesFromAPI = apiData.data.listNotes.items;
+    await Promise.all(
+      notesFromAPI.map(async (note) => {
+        if (note.image) {
+          // const url = await Storage.get(note.name);
+          const { url } = await getUrl({
+            key: note.name,
+            // options: {
+            //   accessLevel?: 'guest' , // can be 'private', 'protected', or 'guest' but defaults to `guest`
+            //   targetIdentityId?: 'XXXXXXX', // id of another user, if `accessLevel` is `guest`
+            //   validateObjectExistence?: false,  // defaults to false
+            //   expiresIn?: 20 // validity of the URL, in seconds. defaults to 900 (15 minutes) and maxes at 3600 (1 hour)
+            //   useAccelerateEndpoint?: true; // Whether to use accelerate endpoint.
+            // },
+          })
+          note.image = url;
+        }
+        return note;
+      })
+    );
     setNotes(notesFromAPI);
   }
 
   async function createNote(event) {
     event.preventDefault();
     const form = new FormData(event.target);
+    const image = form.get("image");
     const data = {
       name: form.get("name"),
       description: form.get("description"),
+      image: image.name,
     };
+    // if (!!data.image) await Storage.put(data.name, image);
+    if (!!data.image) {
+      await uploadData({
+        key: data.name,
+        data: image,
+        options: {
+          onProgress: ({ transferredBytes, totalBytes }) => {
+            if (totalBytes) {
+              console.log(
+                `Upload progress ${
+                  Math.round(transferredBytes / totalBytes) * 100
+                } %`
+              );
+            }
+          }
+        }
+      })
+    }
     await client.graphql({
       query: createNoteMutation,
       variables: { input: data },
@@ -48,9 +89,11 @@ const App = ({ signOut }) => {
     event.target.reset();
   }
 
-  async function deleteNote({ id }) {
+  async function deleteNote({ id, name }) {
     const newNotes = notes.filter((note) => note.id !== id);
     setNotes(newNotes);
+    // await Storage.remove(name);
+    await remove({ key: name });
     await client.graphql({
       query: deleteNoteMutation,
       variables: { input: { id } },
@@ -78,6 +121,12 @@ const App = ({ signOut }) => {
             variation="quiet"
             required
           />
+          <View
+            name="image"
+            as="input"
+            type="file"
+            style={{ alignSelf: "end" }}
+          />
           <Button type="submit" variation="primary">
             Create Note
           </Button>
@@ -96,6 +145,13 @@ const App = ({ signOut }) => {
               {note.name}
             </Text>
             <Text as="span">{note.description}</Text>
+            {note.image && (
+              <Image
+                src={note.image}
+                alt={`visual aid for ${notes.name}`}
+                style={{ width: 400 }}
+              />
+            )}
             <Button variation="link" onClick={() => deleteNote(note)}>
               Delete note
             </Button>
